f1c6cec8abb52407ec5343c7c836fb1172d4a4af
yu-2t <yk.19861210@gmail.com>
2021-03-02 10:39:11 +0900

    textにnameを追加

--- db/migrate/20210216041743_create_texts.rb
+++ db/migrate/20210216041743_create_texts.rb
@@ -1,6 +1,7 @@
 class CreateTexts < ActiveRecord::Migration[6.0]
   def change
     create_table :texts do |t|
+      t.string :name
       t.text :text, null: false
       t.references :topic, null: false
       t.timestamps
